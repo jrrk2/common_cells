f616ece80650260e857f89144a24d459c3fa6600
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2020-01-28 10:39:36 +0000

    Update behavioural RAM model to latest port list and semantics

--- src/sram.sv
+++ src/sram.sv
@@ -16,12 +16,12 @@ module sram #(
     int unsigned NUM_WORDS  = 1024
 )(
    input  logic                          clk_i,
-
+   input  logic                          rst_ni,
    input  logic                          req_i,
    input  logic                          we_i,
    input  logic [$clog2(NUM_WORDS)-1:0]  addr_i,
    input  logic [DATA_WIDTH-1:0]         wdata_i,
-   input  logic [DATA_WIDTH-1:0]         be_i,
+   input  logic [(DATA_WIDTH+7)/8-1:0]   be_i,
    output logic [DATA_WIDTH-1:0]         rdata_o
 );
     localparam ADDR_WIDTH = $clog2(NUM_WORDS);
@@ -37,7 +37,7 @@ module sram #(
                 raddr_q <= addr_i;
             else
             for (int i = 0; i < DATA_WIDTH; i++)
-                if (be_i[i]) ram[addr_i][i] <= wdata_i[i];
+                if (be_i[i/8]) ram[addr_i][i] <= wdata_i[i];
         end
     end
 
